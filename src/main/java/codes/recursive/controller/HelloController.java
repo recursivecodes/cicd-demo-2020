d06b4fedce3c1467ca4a8537a7e0e8400f18d111
recursivecodes <toddraymondsharp@gmail.com>
2020-04-07 16:37:49 -0400

    get instance ocid & public IP

--- src/main/java/codes/recursive/controller/HelloController.java
+++ src/main/java/codes/recursive/controller/HelloController.java
@@ -1,8 +1,11 @@
 package codes.recursive.controller;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.HttpStatus;
+
+import java.util.Map;
 
 @Controller("/hello")
 public class HelloController {
@@ -10,5 +13,14 @@ public class HelloController {
     @Get("/")
     public HttpStatus index() {
         return HttpStatus.OK;
+    }
+
+    @Get("/version")
+    public HttpResponse<Map> getVersion() {
+        return HttpResponse.ok(
+                Map.of(
+                        "version", "0.1"
+                )
+        );
     }
 }
